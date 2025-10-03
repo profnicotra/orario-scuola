09d619d13a0bec4c5ff694fdcc52a700bbb9716c
profnicotra <snicotra@scuola-borsa.it>
2025-10-03 11:40:57 +0200

    Update webapp.py

--- timetable_web/webapp.py
+++ timetable_web/webapp.py
@@ -87,4 +87,4 @@ def files(subpath):
     return send_from_directory(OUTPUTS_DIR, subpath, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
